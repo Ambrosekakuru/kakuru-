ffd6f10dc54c6a8924d54a766d0e054d6cf99aa7
Ambrosekakuru <117356433+Ambrosekakuru@users.noreply.github.com>
2022-11-16 17:41:09 +0300

    Add files via upload

---
+++ kakuru .java
@@ -0,0 +1,73 @@
+
+package code;
+import java.util.Scanner;
+
+public class Ambrose {
+    
+ public static void main(String[] args)
+
+   {
+
+      float[] marks = new float[6];
+
+      float sum=0;
+
+      Scanner scan = new Scanner(System.in);
+
+      
+
+      System.out.print("Enter Marks Obtained in 6 Subjects: ");
+
+      for(int i=0; i<6; i++)
+
+      {
+
+     sum += marks[i];
+
+      }
+
+           float avg = sum/6;
+
+      String grade = "";
+
+       if(avg>=80)
+
+         grade = "A";
+
+
+      else if(avg>=75 && avg<79)
+
+         grade = "B+";
+
+      else if(avg>=70 && avg<74)
+
+         grade = "B";
+
+      else if(avg>=65 && avg<69)
+
+         grade = "C+";
+
+      else if(avg>=60 && avg<64)
+
+         grade = "C";
+
+      else if(avg>=55 && avg<59)
+
+         grade = "D+";
+        
+
+      else if(avg>=50 && avg<54)
+
+         grade = "D";
+
+      
+
+      else if(avg<=49)
+
+         grade = "F";
+
+                System.out.println("\nGrade = " +grade);
+
+   }
+
+}
